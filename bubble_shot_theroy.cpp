305eaff38436cc0c5ac77afe4be8a2069bd1be05
Aditya Jain <adityajainghetal@gmail.com>
2024-12-27 23:42:54 +0530

    bubble shot

---
+++ bubble_shot_theroy.cpp
@@ -0,0 +1,16 @@
+//bubble shot
+
+
+// time complixity
+//  space complixity
+//inplace complixity (space complixity - not used extra array -> subtract and conquer)
+//stable  complixity (same value do not interchange for compration)
+//approach  complixity
+
+
+// best -> 0(n)
+// avg -> 0(n2)
+// wrost -> 0(n2)
+
+
+//space complixity --> do not use new array use (a,b,c)
