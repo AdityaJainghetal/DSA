420e548eb5218da596775e2dab0fb72e7d742edd
Aditya Jain <adityajainghetal@gmail.com>
2025-01-03 21:53:02 +0530

    Quick sort

---
+++ Quick_sort.cpp
@@ -0,0 +1,36 @@
+//Quick sort (binary-algo)	
+//	   |
+//   non-liner algo
+//    (Pivot element -> two  pointer(low value and high value)
+//    recursion also used for formula
+
+
+//rule
+//In a Quick shot we have to keep in mind afollowing conidition
+ 
+// select the pivot element
+// now choose left and right pointer (low and hogh value)
+
+//the left pointer will stop if the value of it greater then pivot element
+//otherwise it will move on 
+
+//(if l > pivot)
+//  Stop
+//else(
+//continue (L++)
+//)
+
+
+
+
+//(if R < pivot)
+//  Stop
+//else(
+//continue (R--)
+//)
+
+
+//if both pointer get stopped the value of Left and Right will be sweaped if they did Cross each other
+//otherwise the value of are will be sweaped with pivot element
+//and the tree will divided into two part, We will follow the recusion till we get single element. 
+
